1797b29203c816118cafb8891f3fc3320ff86349
Gabriel Ethan Gutierrez <gabriel@Gabriels-MacBook-Pro.local>
2020-12-19 21:32:05 -0700

    :D

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,42 @@
+<!-- App.svelte -->
+<!-- Main file, includes routing etc. for the ask.cr website -->
 <script>
+	// Import router
 	import router from 'page'
 
 	// Import routes
-	import HomePage from './HomePage.svelte'
-	import Installation from './Installation.svelte'
-	import Tutorials from './Tutorials.svelte'
-	import Docs from './Docs.svelte'
+	import Home from './Home.svelte'
 
-	// Variables
+	import Questions from './questions/Questions.svelte'
+
+	import PersonalProfile from './profiles/PersonalProfile.svelte'
+
+	import Signup from './forms/Signup.svelte'
+	import Login from './forms/Login.svelte'
+
+	import EULA from './EULA/EULA.svelte'		// REALLY sucks right now
+
+	// Set page variable
 	let page
+	let params
 
 	// Set up pages to watch for
-	router('/', () => page = HomePage)
-	router('/install', () => page = Installation)
-	router('/tutorials', () => page = Tutorials)
-	router('/docs', () => page = Docs)
+	router('/', () => page = Home)
+	router('/signup', () => page = Signup)
+	router('/login', () => page = Login)
+	router('/terms', () => page = EULA)
+	router('/questions', () => page = Questions)
+	router('/profile', () => {
+		page = PersonalProfile
+		params = {
+			name: 'gaetgu',
+			title: 'Svelte Dude',
+			bio: 'Just a normal ICE agent from Iceland.'
+		}
+	}) 
 
 	// Set up the router to start and actively watch for changes
 	router.start()
 </script>
 
-<svelte:component this="{page}" />
+<svelte:component this="{page}" {params} />
--- src/Installation.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    import NavBar from './NavBar.svelte'
-    import InstallCard from './InstallCard.svelte'
-</script>
-
-<NavBar current="Installation" />
-
-<InstallCard />
